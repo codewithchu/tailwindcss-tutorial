bf3f8e063722ae1df0bbf7fa4e3e8079001f959d
Arthur Chu <cwc@arthurchu.ca>
2020-10-07 12:47:44 -0400

    02-setup install

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
+    },
+    purge: [],
+    theme: {
+        extend: {
+            colors: {
+                'cwc-color': 'transparent',
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
