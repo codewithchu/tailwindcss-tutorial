bd877fb2e739a5007e8b789d6ac9513343c10846
Arthur Chu <cwc@arthurchu.ca>
2020-10-09 12:58:25 -0400

    04-Typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     },
     purge: [],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                'extra-condensed': ['Saira Extra Condensed', 'sans-serif'],
+            },
+            fontSize: {
+                '3xs': '0.6rem',
+                '2xs': '0.7rem',
+                xs: '0.8rem',
+            },
+        },
     },
     variants: {},
     plugins: [],
