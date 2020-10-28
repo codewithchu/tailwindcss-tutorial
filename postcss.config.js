04a392d07201392d53e90972e280fef25744a19a
codewithchu <cwc@arthurchu.ca>
2020-10-28 18:10:16 -0400

    13 Purge Css

--- postcss.config.js
+++ postcss.config.js
@@ -1,35 +1,33 @@
-const cssnano = require('cssnano');
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    // Paht and File Extentions
-    content: [
-        './public/**/*.html',
-        './public/**/*.js',
-        //'./public/**/*.vue',
-        //'./public/**/*.jsx',
-        // etc.
-    ],
+const cssnano = require("cssnano");
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: [
+    "./public/**/*.html",
+    "./public/**/*.js",
+    // etc.
+  ],
 
-    // This is the function used to extract class names from your templates
-    defaultExtractor: (content) => {
-        // Capture as liberally as possible, including things like `h-(screen-1.5)`
-        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+  // This is the function used to extract class names from your templates
+  defaultExtractor: (content) => {
+    // Capture as liberally as possible, including things like `h-(screen-1.5)`
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
 
-        // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
 
-        return broadMatches.concat(innerMatches);
-    },
+    return broadMatches.concat(innerMatches);
+  },
 });
 
 module.exports = {
-    plugins: [
-        // ...
-        require('tailwindcss'),
-        cssnano({
-            preset: 'default',
-        }),
-        require('autoprefixer'),
-        purgecss,
-        // ...
-    ],
+  plugins: [
+    // ...
+    require("tailwindcss"),
+    cssnano({
+      preset: "default",
+    }),
+    require("autoprefixer"),
+    purgecss,
+    // ...
+  ],
 };
